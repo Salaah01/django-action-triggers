19e36c9e3c3831001869ce8e88b1382427ce6a89
Salaah Amin <salaah01@users.noreply.github.com>
2024-08-11 14:23:33 +0100

    updated app name and enabled manage.py ability to import the package

--- playground/manage.py
+++ playground/manage.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 
 
+# In the parent directory there exists an app that we want to be able to
+# use. So we need to add the parent directory to the PYTHONPATH.
+sys.path.append(os.path.join(os.path.dirname(__file__), "..",))
+
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'playground.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "playground.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +24,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
