d0f73aa05d1438730945c82129f32e17aa9606c1
Salaah Amin <salaah01@users.noreply.github.com>
2024-08-12 19:49:36 +0100

    updated docstring for parse_payload

--- action_triggers/payload/parser.py
+++ action_triggers/payload/parser.py
@@ -10,8 +10,13 @@ def parse_payload(instance: Model, payload: str) -> _t.Union[dict, str]:
     object. If the parsed object is JSON serializable, it will be returned as
     a dictionary, otherwise it will be returned as a string.
 
-    :param instance: The model instance to parse the payload for.
-    :param payload: The payload to parse.
+    Args:
+        instance: The model instance to parse the payload for.
+        payload: The payload to parse.
+
+    Returns:
+        The parsed payload - either a dictionary or a string depending on
+        whether the parsed object is JSON serializable.
     """
 
     parsed = Template(payload).render(Context({"instance": instance}))
