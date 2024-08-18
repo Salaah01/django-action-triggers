aeda71eb3f87d6975ea8b304afb2fb7a5aeb4fd8
Salaah Amin <salaah01@users.noreply.github.com>
2024-08-18 18:23:42 +0100

    docstring

--- action_triggers/dispatch.py
+++ action_triggers/dispatch.py
@@ -9,9 +9,21 @@ from action_triggers.webhooks import WebhookProcessor
 
 
 def handle_action(config: Config, instance: Model) -> None:
+    """Handle the action for the given config and instance.
+
+    For each webhook and message broker queue associated with the config,
+    the payload is generated and sent to the respective handlers.
+
+    :param config: The configuration object.
+    :param instance: The model instance which triggered the action.
+    :return: None
+    """
+
     payload_gen = get_payload_generator(config)
     payload = payload_gen(instance)
+
     for webhook in config.webhooks.all():
         WebhookProcessor(webhook, payload).process()
+
     for msg_broker_queue in config.message_broker_queues.all():
         process_msg_broker_queue(msg_broker_queue, payload)
