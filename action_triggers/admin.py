d25ca3dce286c5ac0a8e86c57c1f634309b2bcea
Salaah Amin <salaah01@users.noreply.github.com>
2024-08-20 20:27:41 +0100

    docs

--- action_triggers/admin.py
+++ action_triggers/admin.py
@@ -4,22 +4,30 @@ from action_triggers import models as action_triggers_models
 
 
 class WebhookInline(admin.StackedInline):
+    """Support for inlining `Webhook` objects."""
+
     model = action_triggers_models.Webhook
     extra = 0
 
 
 class MessageBrokerQueueInline(admin.StackedInline):
+    """Support for inlining `MessageBrokerQueue` objects."""
+
     model = action_triggers_models.MessageBrokerQueue
     extra = 0
 
 
 class ConfigSignalInline(admin.TabularInline):
+    """Support for inlining `ConfigSignal` objects."""
+
     model = action_triggers_models.ConfigSignal
     extra = 0
 
 
 @admin.register(action_triggers_models.Config)
 class ConfigAdmin(admin.ModelAdmin):
+    """Admin interface for the `Config` model."""
+
     list_display = ("id", "created_on", "created_by", "active")
     list_filter = ("active", "created_on", "content_types")
     search_fields = ("id", "created_by__username")
@@ -32,6 +40,8 @@ class ConfigAdmin(admin.ModelAdmin):
 
 @admin.register(action_triggers_models.Webhook)
 class WebhookAdmin(admin.ModelAdmin):
+    """Admin interface for the `Webhook` model."""
+
     list_display = ("id", "config", "url", "http_method")
     list_filter = ("http_method",)
     search_fields = ("config__id",)
@@ -40,6 +50,8 @@ class WebhookAdmin(admin.ModelAdmin):
 
 @admin.register(action_triggers_models.MessageBrokerQueue)
 class MessageBrokerQueueAdmin(admin.ModelAdmin):
+    """Admin interface for the `MessageBrokerQueue` model."""
+
     list_display = ("id", "name", "config")
     search_fields = ("config__id",)
     autocomplete_fields = ("config",)
@@ -47,6 +59,8 @@ class MessageBrokerQueueAdmin(admin.ModelAdmin):
 
 @admin.register(action_triggers_models.ConfigSignal)
 class ConfigSignalAdmin(admin.ModelAdmin):
+    """Admin interface for the `ConfigSignal` model."""
+
     list_display = ("id", "config", "signal")
     list_filter = ("signal",)
     search_fields = ("config__id",)
