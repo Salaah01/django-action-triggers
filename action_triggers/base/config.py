72b623f7d630851e4a993cada5ffe554bd15645d
Salaah Amin <salaah01@users.noreply.github.com>
2024-10-03 22:47:36 +0100

    formatting

--- action_triggers/base/config.py
+++ action_triggers/base/config.py
@@ -4,8 +4,8 @@ Note: This is not used in the current implementation. It is a work in progress
 and a placeholder for future work.
 """
 
-from abc import ABC, abstractproperty  # pragma: no cover
 import typing as _t  # pragma: no cover
+from abc import ABC, abstractproperty  # pragma: no cover
 
 from action_triggers.base.validation import (  # pragma: no cover
     ConfigValidationBase,
