af2c38ef24f020559df3a579d531932785c0d674
Salaah Amin <salaah01@users.noreply.github.com>
2024-10-10 01:43:03 +0100

    bumped version and updated project description

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,9 @@ copyright = "2024, Salaah01"
 author = "Salaah01"
 
 # The short X.Y version
-version = "0.5.0"
+version = "0.5.1"
 # The full version, including alpha/beta/rc tags
-release = "0.5.0"
+release = "0.5.1"
 
 
 # -- General configuration ---------------------------------------------------
